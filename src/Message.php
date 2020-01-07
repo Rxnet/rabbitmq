bd145b19031265c39a7c5fb9e19ed211ed1ebfdd
Jérémy Marodon <marodon.jeremy@gmail.com>
2020-01-07 14:36:55 +0100

    :boom: Rework of Message entity
    Encapsulate properties
    Manage correctly falsy promise
    Remove labels management in headers

--- src/Message.php
+++ src/Message.php
@@ -10,97 +10,121 @@ namespace Rxnet\RabbitMq;
 
 use Bunny\Channel;
 use Bunny\Message as BaseMessage;
+use React\Promise\PromiseInterface;
 use Rx\Observable;
 
 class Message
 {
     /** @var string */
-    public $consumerTag;
+    protected $consumer_tag;
     /** @var int */
-    public $deliveryTag;
+    protected $delivery_tag;
     /** @var bool */
-    public $redelivered;
+    protected $redelivered;
     /** @var string */
-    public $exchange;
+    protected $exchange;
     /** @var string */
-    public $routingKey;
+    protected $routing_key;
     /** @var array */
-    public $headers;
-
+    protected $headers = [];
+    /** @var string */
+    protected $content;
+    /** @var Channel */
     protected $channel;
-    protected $message;
-    protected $trait;
-
-    protected $data;
-    protected $labels = [];
+    /** @var BaseMessage  */
+    protected $base_message;
 
     const HEADER_TRIED = 'tried';
-    const HEADER_LABELS = 'labels';
+    const HEADER_DELAY = 'x-delay';
 
-    public function __construct(Channel $channel, BaseMessage $message)
+    public function __construct(Channel $channel, BaseMessage $base_message)
     {
         $this->channel = $channel;
-        $this->message = $message;
+        $this->base_message = $base_message;
 
-        $this->consumerTag = $message->consumerTag;
-        $this->deliveryTag = $message->deliveryTag;
-        $this->redelivered = $message->redelivered;
-        $this->exchange = $message->exchange;
-        $this->routingKey = $message->routingKey;
-        $this->headers = $message->headers;
+        $this->consumer_tag = $base_message->consumerTag;
+        $this->delivery_tag = $base_message->deliveryTag;
+        $this->redelivered = $base_message->redelivered;
+        $this->exchange = $base_message->exchange;
+        $this->routing_key = $base_message->routingKey;
+        $this->headers = $base_message->headers;
 
         $this->headers[self::HEADER_TRIED] = isset($this->headers[self::HEADER_TRIED]) ? $this->headers[self::HEADER_TRIED] + 1 : 1;
 
-        $this->data = $message->content;
-
-        $this->labels = $this->headers[self::HEADER_LABELS] ?? null;
-        try {
-            $this->labels = json_decode($this->labels, true) ?? [];
-        } catch (\InvalidArgumentException $e) {
-            $this->labels = [];
-        }
+        $this->content = $base_message->content;
     }
 
-    public function getRoutingKey(): string
+    public function channel(): Channel
     {
-        return $this->routingKey;
+        return $this->channel;
     }
 
-    public function getData()
+    public function message(): BaseMessage
     {
-        return $this->data;
+        return $this->base_message;
     }
 
-    public function getHeader($header, $default = null)
+    public function consumerTag(): string
+    {
+        return $this->consumer_tag;
+    }
+
+    public function deliveryTag(): int
+    {
+        return $this->delivery_tag;
+    }
+
+    public function redelivered(): bool
+    {
+        return $this->redelivered;
+    }
+
+    public function exchange(): string
+    {
+        return $this->exchange;
+    }
+
+    public function routingKey(): string
+    {
+        return $this->routing_key;
+    }
+
+    public function content(): string
+    {
+        return $this->content;
+    }
+
+    public function headers(): array
+    {
+        return $this->headers;
+    }
+
+    public function header(string $header, $default = null)
     {
         return $this->headers[$header] ?? $default;
     }
 
-    public function getLabels(): array
+    public function addHeader(string $header, $value): self
     {
-        return $this->labels;
-    }
+        if (!is_scalar($value)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Header must be a scalar value, %s given',
+                gettype($value)
+            ));
+        }
 
-    public function getLabel($label, $default = null)
-    {
-        return $this->labels[$label] ?? $default;
-    }
-
-    public function setLabel($label, $value): Message
-    {
-        $this->labels[$label] = $value;
+        $this->headers[$header] = $value;
 
         return $this;
     }
 
-    public function getRawContent()
-    {
-        return $this->message->content;
-    }
-
     public function ack(): Observable
     {
-        $promise = $this->channel->ack($this->message);
+        $promise = $this->channel->ack($this->base_message);
+
+        if (!$promise instanceof PromiseInterface) {
+            throw new \RuntimeException('Promise can\'t be returned');
+        }
 
         return Observable::fromPromise($promise);
     }
@@ -110,48 +134,47 @@ class Message
      */
     public function nack($requeue = true): Observable
     {
-        $promise = $this->channel->nack($this->message, false, $requeue);
+        $promise = $this->channel->nack($this->base_message, false, $requeue);
+
+        if (!$promise instanceof PromiseInterface) {
+            throw new \RuntimeException('Promise can\'t be returned');
+        }
 
         return Observable::fromPromise($promise);
     }
 
     public function reject($requeue = true): Observable
     {
-        $promise = $this->channel->reject($this->message, $requeue);
+        $promise = $this->channel->reject($this->base_message, $requeue);
+
+        if (!$promise instanceof PromiseInterface) {
+            throw new \RuntimeException('Promise can\'t be returned');
+        }
 
         return Observable::fromPromise($promise);
     }
 
-    protected function prepareHeaders($additionalHeaders = []): array
-    {
-        $headers = $this->headers;
-        if (isset($headers[self::HEADER_LABELS])) {
-            unset($headers[self::HEADER_LABELS]);
-        }
-        if ($this->labels) {
-            $headers[self::HEADER_LABELS] = json_encode($this->labels);
-        }
-
-        return array_merge(
-            $headers,
-            $additionalHeaders
-        );
-    }
-
     public function retryLater($delay, $exchange = 'direct.delayed'): Observable
     {
-        $headers = $this->prepareHeaders(['x-delay' => $delay]);
+        $headers = array_merge(
+            $this->headers(),
+            [self::HEADER_DELAY => $delay]
+        );
 
         return $this->reject(false)
             ->flatMap(function () use ($headers, $exchange) {
-                return Observable::fromPromise(
-                    $this->channel->publish(
-                        $this->getData(),
-                        $headers,
-                        $exchange,
-                        $this->routingKey
-                    )
+                $promise = $this->channel->publish(
+                    $this->content(),
+                    $headers,
+                    $exchange,
+                    $this->routing_key
                 );
+
+                if (!$promise instanceof PromiseInterface) {
+                    throw new \RuntimeException('Promise can\'t be returned');
+                }
+
+                return Observable::fromPromise($promise);
             });
     }
 
@@ -159,14 +182,18 @@ class Message
     {
         return $this->reject(false)
             ->flatMap(function () {
-                return Observable::fromPromise(
-                    $this->channel->publish(
-                        $this->getData(),
-                        $this->prepareHeaders(),
-                        $this->exchange,
-                        $this->routingKey
-                    )
+                $promise = $this->channel->publish(
+                    $this->content(),
+                    $this->headers(),
+                    $this->exchange,
+                    $this->routing_key
                 );
+
+                if (!$promise instanceof PromiseInterface) {
+                    throw new \RuntimeException('Promise can\'t be returned');
+                }
+
+                return Observable::fromPromise($promise);
             });
     }
 }
